0aa699dc5b718b30259bd70894806e93e54f6c66
RCid <rcid@myring.io>
2015-11-03 15:20:02 -0500

    New class Preparer and refactoring

--- CSV2MR.py
+++ CSV2MR.py
@@ -1,236 +1,30 @@
-import csv
-import requests
-
-class CSV2MR:
-    
-
-    def __init__(self):
-        self.AI_agent = 'AVISPA'
-        self.csv2mr_map = {}
-
-    @staticmethod
-    def rn_step1(AI_agent):
-
-        return raw_input('[%s]: What is the name of the CSV file you want to import? \n[YOU]: '%AI_agent)
-    
-    
-    def rn_step2(self,AI_agent):
-
-        return raw_input('[%s]: What is the URL of the Ring? \n[YOU]: '%AI_agent)
-    
-    
-    def mmf_step1(self,AI_agent):
-
-        return raw_input('[%s]: Do you want to enter a new language?[yes/no] \n[YOU]: '%AI_agent)
-
-    
-    def mmf_step2(self,langs,AI_agent):
-
-        return raw_input('[%s]: What language you want to map?[%s] \n[YOU]: '%(AI_agent,'/'.join(langs)))
-
-    
-    def mmf_step3(self,f,l,columns,AI_agent):
-
-        return raw_input('[%s]: What CSV column matches with Ring field "%s.%s" ?[1-%s]  \n[YOU]: '%(
-                                   AI_agent,
-                                   f['FieldLabel'],
-                                   l,
-                                   len(columns)))
-
-    def mrf_step1(self,f,columns,AI_agent):
-
-        return raw_input('[%s]: What CSV column matches with ring field "%s" ?[1-%s] \n[YOU]: '% (
-                                                           AI_agent,
-                                                           f['FieldLabel'],
-                                                           len(columns)))
-        
-
-    def run(self,
-            xrn_step1=rn_step1,
-            xrn_step2=rn_step2,
-            xmmf_step1=mmf_step1,
-            xmmf_step2=mmf_step2,
-            xmmf_step3=mmf_step3, 
-            xmrf_step1=mrf_step1):
-
-        print('[%s]: So, you want to import data from a CSV file to a Ring right?  '%self.AI_agent)
-
-        csv_filename = xrn_step1(self.AI_agent)
-        columns = self.get_csv_header(csv_filename)
-
-        ring_url = xrn_step2(self.AI_agent)
-        ring_url_parts = ring_url.split('?')
-        ring,fields = self.get_ring_schema(ring_url_parts[0]+'?schema=1')
-
-        print('[%s]: Given the following CSV Columns:'%self.AI_agent)
-        print(self.columns_menu_string(columns))
-
-        
-        for f in fields:
-            if f['FieldMultilingual']:
-                self.map_multilingual_field(f,
-                                            columns,
-                                            xmmf_step1=xmmf_step1,
-                                            xmmf_step2=xmmf_step2,
-                                            xmmf_step3=xmmf_step3)
-            else:
-                self.map_regular_field(f,columns,xmrf_step1=xmrf_step1)
-
-
-        print('[%s]: This is your map:'%self.AI_agent)
-        print(self.csv2mr_map)
-
-
-        
-
-        return self.csv2mr_map
-
-
-
-
-
-
-    def map_regular_field(self,f,columns,xmrf_step1=mrf_step1):
-
-        self.csv2mr_map[f['FieldId']] = xmrf_step1(f,columns,self.AI_agent)
-
-
-    def map_multilingual_field(self,f,columns,xmmf_step1=None,xmmf_step2=None,xmmf_step3=None):
-
-        print('[%s]: "%s" is a Multilingual field '%(self.AI_agent,f['FieldLabel']))
-        langs = ['en','sp','it','fr']
-        lang_dict = {}
-        next_lang = True
-        while next_lang:
-            r = xmmf_step1(self.AI_agent)
-            if str(r[0]).lower() == 'y': 
-                l = xmmf_step2(langs,self.AI_agent)
-                if l in langs:
-                    c = xmmf_step3(f,l,columns,self.AI_agent)
-                    lang_dict[l] = c
-                else:
-                    print("[%s]: I don't recognize that language"%self.AI_agent)
-            elif str(r[0]).lower() == 'n':
-                next_lang = False
-
-
-        self.csv2mr_map[f['FieldId']] = lang_dict
-
-    def columns_menu_string(self,columns):
-
-        i = 0
-        out = ''
-        for c in columns:
-            i += 1
-            out += ' %s)%s'%(i,c)
-
-        return out
-
-
-    def retry_get_csv_header(self,raw_input):
-
-        csv_filename = raw_input('[%s]: What is the name of the CSV file you want to import? \n[YOU]: '%self.AI_agent)
-        return self.get_csv_header(csv_filename)
-
-        
-    def get_csv_header(self,csv_filename,retry_raw_input=raw_input):
-
-        try:
-
-            with open(csv_filename,'rb') as f:
-                self.reader = csv.reader(f)
-
-                for row in self.reader:
-                    #print(row)
-                    csv_header=[]
-                    count = 0
-
-                    for r in row:
-                        #print(type(r))
-
-                        if r != '':
-                            csv_header.append(r)
-                            count += 1
-                    
-                    if count != 0:
-                        break
-
-                print('[%s]: %s columns detected: '%(self.AI_agent,len(csv_header)))
-                print(', '.join(csv_header))
-
-                return csv_header
-
-        except(IOError):
-
-            print('[%s]: There was a problem opening that file. '%self.AI_agent)
-            print('[%s]: It should live in the same folder as this script. '%self.AI_agent)
-            return self.retry_get_csv_header(retry_raw_input)
-
-    def retry_get_ring_schema(self,raw_input):
-        
-        ring_url = raw_input('[%s]: What is the URL of the Ring? \n[YOU]: '%self.AI_agent)
-        return self.get_ring_schema(ring_url)
-
-
-    def get_ring_schema(self,ring_url,retry_raw_input=raw_input):
-
-        try:
-
-            r = requests.get(ring_url)
-
-            if r.status_code == requests.codes.ok:
-
-                #print(r.json())
-                fields = r.json()['fields']
-                rings = r.json()['rings']
-
-                print('[%s]: Got it!'%self.AI_agent)
-                print('[%s]: %s fields detected: '%(self.AI_agent,len(fields)))   
-                print(', '.join([field['FieldLabel'] for field in fields]))
-
-                return rings,fields
-
-            else:
-                raise
-
-        except(ValueError):
-            print('[%s]: There was a problem with that URL. Do you want to try another URL?'%self.AI_agent)
-            return self.retry_get_ring_schema(retry_raw_input)
-
-        except(requests.exceptions.MissingSchema):
-            print('[%s]: That is not a valid URL. Do you want to try another URL?'%self.AI_agent)
-            return self.retry_get_ring_schema(retry_raw_input)
-
-        except(requests.exceptions.ConnectionError):
-            print('[%s]: Connection refused. Do you want to try another URL?'%self.AI_agent)
-            return self.retry_get_ring_schema(retry_raw_input)
-
-
-    def post_item(self,item):
-
-        
-        map = {u'cc784d6f': '1', u'4f9a647f': '2', u'2aa59797': '11', u'8e5b7eb1': '4', u'1a3ffd3b': {'fr': '9', 'en': '8', 'it': '5', 'sp': '6'}, u'd88555ed': '3'}
-
-        for 
-
-        payload = {'x':1}
-
-        requests.post(url,payload)
-
-
-
+from Mapper import Mapper
+from Preparer import Preparer
 
 
 if __name__ == '__main__':
 
-    c2r = CSV2MR()
-    c2r.run(xrn_step1=c2r.rn_step1,
-            xrn_step2=c2r.rn_step2,
-            xmmf_step1=c2r.mmf_step1,
-            xmmf_step2=c2r.mmf_step2,
-            xmmf_step3=c2r.mmf_step3, 
-            xmrf_step1=c2r.mrf_step1)
+    mp= Mapper()
+    rmap = mp.run(xrn_step1=mp.rn_step1,
+            xrn_step2=mp.rn_step2,
+            xmmf_step1=mp.mmf_step1,
+            xmmf_step2=mp.mmf_step2,
+            xmmf_step3=mp.mmf_step3, 
+            xmrf_step1=mp.mrf_step1)
 
-    
+    pr = Preparer()
+    filelist = ['TT1.csv']
+    openedfiles = pr.opener(filelist)
+    rows = pr.cat(openedfiles)
+    itemlist = pr.populate(rows,rmap)
 
-    
+    count = 0
+
+    #itemlist is a generator. 
+
+    for item in itemlist:
+        count += 1
+        print(item)
+
+    print('Total Items:%s'%count)
+   
--- unittest_CSV2MR.py
+++ unittest_CSV2MR.py
@@ -1,17 +1,18 @@
 #unitest_CSV2MR.py
 
 import unittest
-from CSV2MR import CSV2MR
+from CSV2MR import Mapper,Preparer
 
-class TestFunctionality(unittest.TestCase):
+class TestMapper(unittest.TestCase):
 
     def setUp(self):
 
-        self.cm = CSV2MR()
-        self.csv_filename = 'TT1.csv'
-        self.incorrect_csv_filename = 'TTxxx.csv'
+        self.cm = Mapper()
+        self.csv_filename = 'fixtures/TT1.csv'
+        self.incorrect_csv_filename = 'fixtures/TTxxx.csv'
+        #TO DO: mock this call
         self.ring_url = 'https://avispa.myring.io/_api/teamamerica/vendortranslations?schema=1'
-        self.incorrect_ring_url = 'https://avispax.myring.io/_api/teamamerica/vendortranslations?schema=1'
+        self.incorrect_ring_url = 'https://avispax.myring.io/_api/xxx/yyy?schema=1'
         self.invalid_ring_url = 'invalidurl'
         self.columns = ['Option_1','Option_2']
         self.f = {'FieldLabel':'Name','FieldId':'1234qwerty'}
@@ -105,6 +106,46 @@ class TestFunctionality(unittest.TestCase):
             xmrf_step1=self.mrf_step1)
         self.assertEquals(result,{u'cc784d6f': '1', u'4f9a647f': '1', u'2aa59797': '1', u'8e5b7eb1': '1', u'1a3ffd3b': {'en': '1'}, u'd88555ed': '1'})
     
+
+class TestPreparer(unittest.TestCase):
+
+    def setUp(self):
+        self.pr = Preparer()
+        self.csv_filename = 'fixtures/TT1.csv'
+        self.rows = [['936', 'YUMA', 'QUALITY INN & SUITES YUMA', 'The Comfort Inn hotel is conveniently located off Interstate 8 and U.S. Route 95, minutes from downtown Yuma, the Yuma International Airport, Old Town Yuma and the Marine Corps Air Station Yuma. This Yuma, AZ hotel is also close to the Yuma Palms Regional Center, the Yuma Proving Ground and Paradise Casino....', '', '', '', '', '', '', '', ''],
+        ['15952', 'BROOKLYN', 'WYTHE HOTEL', '', "I Wythe Hotel nacque con la scoperta di una fabbrica sul lungofiume di Williamsburg. L'edificio risale al 1901 ed \xc3\xa8 stato convertito in un hotel da 70 camere. L'hotel ha un bar panoramico, una palestra, un parcheggio ed una sala cinema. Tutte le 70 camere dell'albergo sono dotate di accesso internet Wi-Fi gratuito, TV LED a schermo piatto, minibar rifornito interamente di prodotti locali e arredamento realizzato localmente. Le stazioni metropolitane per le linee L e G sono a breve distanza dall'albergo e Manhattan \xc3\xa8 facilmente raggiungibile con un breve viaggio in metropolitana.", '', '', 'Wythe Hotel started with the discovery of a factory on the Williamsburg waterfront. The building was constructed in 1901 and has been converted into a 70-room hotel. The Hotel has a rooftop bar, a gym, parking facility and a Screening Room. All the 70 rooms offer complimentary high speed Wi-Fi, flat screen LED HDTV, full-service locally sourced minibar and locally made furniture.\nThe subway stations for the lines L an G are nearby and is possible to reach Manhattan with a short ride on the subway.', '', '', '', '']]
+        self.rmap = {u'cc784d6f': '1', u'4f9a647f': '2', u'2aa59797': '11', u'8e5b7eb1': '4', u'1a3ffd3b': {'fr': '9', 'en': '8', 'it': '5', 'sp': '6'}, u'd88555ed': '3'}
+    
+
+    def test_opener(self):
+        l = [self.csv_filename]
+        openedfiles = self.pr.opener(l)
+        f = openedfiles.next()  
+        self.assertEquals(type(f),file)
+
+    def test_cat(self):
+        openedfileslist = [open(self.csv_filename)]
+        openedfiles = (f for f in openedfileslist)
+        lines = self.pr.cat(openedfiles)
+        lines.next() # First line comes empty
+        line = lines.next()
+        self.assertEquals(line[1],'City')
+
+    def test_populate(self):
+        itemlist = self.pr.populate(self.rows,self.rmap)
+        item = itemlist.next()
+        self.assertEquals(item['cc784d6f'],'936')
+
+    def test_populate_with_languages(self):
+        itemlist = self.pr.populate(self.rows,self.rmap)
+        itemlist.next() #Let the first item pass
+        item = itemlist.next()
+        self.assertEquals(item['1a3ffd3b']['it'][:20],'I Wythe Hotel nacque')
+
+
+
+
+
 
 
 if __name__ == '__main__':
